72d8247c8aba81098b5011cbe31127a4694469f9
pfilip <petr.filip@tix.cz>
2019-04-06 07:24:46 +0200

    2 - config improved

--- simpleshop-inventory-microservice/src/main/java/cz/upce/inpia/simpleshop/inventory/controller/RestInventoryController.java
+++ simpleshop-inventory-microservice/src/main/java/cz/upce/inpia/simpleshop/inventory/controller/RestInventoryController.java
@@ -1,11 +1,20 @@
 package cz.upce.inpia.simpleshop.inventory.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/inventory")
 @CrossOrigin
 public class RestInventoryController {
+
+    @Value("${inpia.inventory.test:default}")
+    private String inpiaTestProperty;
+
+    @GetMapping
+    public String defaultMethod() {
+        return inpiaTestProperty;
+    }
 
     @GetMapping("{productId}")
     public Integer getProductById(@PathVariable Integer productId) {
